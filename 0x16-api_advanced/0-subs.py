e8b756fc2aaaf151f2e6f8fa90f42529feb98f2b
Abdelrahman-Muhammad <97410751+Abdelrahman-Muhammad@users.noreply.github.com>
2024-02-08 15:59:49 -0800

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,15 @@
+#!/usr/bin/python3
+""" module 0-subs.py """
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """Returns the number of subscribers for a given subreddit"""
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an exception for non-200 status codes
-
-        results = response.json().get("data")
-        subscribers = results.get("subscribers")
-        print(subscribers)  # Print the subscriber count
-        return subscribers
-
-    except requests.exceptions.RequestException as e:
-        print("Error fetching subreddit data:", e)
+    headers = {"User-Agent": "Test"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    else:
         return 0
