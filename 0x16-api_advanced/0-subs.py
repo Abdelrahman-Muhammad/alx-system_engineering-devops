15910e30337b5f13faf1fd187fa85139ac175acf
Abdelrahman-Muhammad <97410751+Abdelrahman-Muhammad@users.noreply.github.com>
2024-02-08 15:54:12 -0800

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,21 @@
-#!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
-
-from requests import get
-
+import requests
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
     try:
-        return results.get('data').get('subscribers')
+        user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+        response = requests.get(url, headers=user_agent)
 
-    except Exception:
-        return 0
+        if response.status_code == 200:
+            try:
+                results = response.json()
+                return results.get('data').get('subscribers')
+            except json.JSONDecodeError as e:
+                print("Invalid JSON response:", e)
+        else:
+            print("API request failed with status code:", response.status_code)
+
+    except requests.exceptions.RequestException as e:
+        print("Network error:", e)
+
+    return 0
