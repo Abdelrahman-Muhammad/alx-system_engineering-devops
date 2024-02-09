92ed32bac333426062cbd13c94c785527910a639
Abdelrahman Muhammad <boodyonline1@gmail.com>
2024-02-08 16:53:12 -0800

    4

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and returns the number of subscribers(not active
-users, total subscribers) for a given subreddit. if an invalid subreddit
-is given, the function should return 0.
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """queries the Reddit API"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-        }
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-    results= response.json().get("data")
-    return results.get("subscribers")
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
