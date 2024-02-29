9ebb3c49f9de8a0871328d0bd50070df4fd3efac
Abdelrahman-Muhammad <97410751+Abdelrahman-Muhammad@users.noreply.github.com>
2024-02-29 06:16:14 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""
-This module contains a function that queries the Reddit API and returns the
-number of subscribers for a given subreddit
-"""
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
-    # Set the custom User-Agent header
-    headers = {"User-Agent": "Bing"}
-    # Make a GET request to the subreddit's about.json endpoint
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    # Check the status code and the response data
-    if response.status_code == 200:
-        data = response.json().get("data")
-        if data:
-            # Return the number of subscribers
-            return data.get("subscribers", 0)
-    # Return 0 for invalid subreddit or other errors
-    return 0
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")
